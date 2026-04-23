38c432dab4eefdeb4322afd9e988c32b25c03708
kavy-gli <beenachudasama44@gmail.com>
2026-04-23 12:20:08 +0530

    Add program to calculate third angle of a triangle

---
+++ PR-1/Que-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+float a,b,third,sum;
+   
+printf("Enter first angle : ");
+
+   scanf("%f",&a);
+
+   printf("Enter second angle : ");
+   
+ scanf("%f",&b);
+ 
+  sum=a+b;
+ 
+   third=180-sum;
+
+   printf("%f",third);
+return 0;
+
+}
