a73f3c996c420d74a152c8ae9885c23b01bdf466
kavy-gli <beenachudasama44@gmail.com>
2026-04-23 12:17:10 +0530

    Calculate area of a circle based on user input

--- PR-1/Que-1.c
+++ PR-1/Que-1.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
-int main(){
-  return 0;
-}
+
+int main()
+ {
+
+    float r,pi=3.14,area;
+
+    printf("Enter your r : ");
+
+    scanf("%f",&r);
+
+    area=2*pi*r;
+
+    printf("%f\n",area);
+
+    return 0;
